1f3a2f980ab019b9848e879fd1a80031e9fc6224
krockode <krockode@gmail.com>
2013-02-06 18:41:48 +1000

    use os.path

--- test/test_cucumber_completion.py
+++ test/test_cucumber_completion.py
@@ -1,19 +1,20 @@
-import os
+from os import listdir, path
+
 
 import sublime, sublime_plugin
 from CucumberFeatureAutocomplete import CucumberFeatureAutocomplete
 
 completer = CucumberFeatureAutocomplete()
-
-# test examples found in the examples directory
+ex = 'examples'
+# test all example step definitions found in the examples directory
 def test_examples():
-    tests = [d for d in os.listdir('examples') if not d.endswith('expected')]
+    tests = [d for d in listdir(ex) if path.isdir(path.join(ex, d))]
     for d in tests:
         yield check_examples, d
 
 def check_examples(d):
     message = "expectations failed for completions in {0}; found: '{1}'"
-    test_dir = 'examples/{0}'.format(d,)
-    expected = set(open('{0}.expected'.format(test_dir,)).read().splitlines())
+    test_dir = path.join(ex, d)
+    expected = set(open('{0}.expected'.format(test_dir)).read().splitlines())
     found = set(completer.find_completions([test_dir]))
     assert expected == found, message.format(d, "', '".join(found))
